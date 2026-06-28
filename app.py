be717fac21d7799caf08a65b294c956dd0d20e31
Khushi Singhal <khushisinghal59@gmail.com>
2026-06-28 22:36:24 +0530

    Prepare project for deployment

--- app.py
+++ app.py
@@ -261,10 +261,9 @@ def history():
     return render_template('history.html', predictions=predictions)
 
 
-# ---------------------------------------------------------------------------
-# Create Database Tables & Run App
-# ---------------------------------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
